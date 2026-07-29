dc0d0f9dd2cf271f61623ff605ec994acead3f2a
jessica <jessicadhamala20@gmail.com>
2026-07-29 16:00:46 +0545

    Update application constants

--- src/configs/constant.ts
+++ src/configs/constant.ts
@@ -7,6 +7,8 @@ export const DUMMY: string = process.env.DUMMY || "Dummy Export";
 export const MONGODB_URL: string = process.env.MONGODB_URL || "mongodb://localhost:27017/class-36a-db"; // default MongoDB URL
 export const SECRET_KEY: string =
     process.env.SECRET_KEY || "merosecretkey";
+export const EMAIL_USER: string = process.env.EMAIL_USER || "jessicad10@gmail.com";
+export const EMAIL_PASS: string = process.env.EMAIL_PASS || "cnkecvfckziitrid";
 
 // same as 
 // export {
