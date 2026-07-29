15e5f3a7c0394bb010d1d0ab8d7ba7c403c8bb22
jessica <jessicadhamala20@gmail.com>
2026-07-29 16:02:25 +0545

    Update user model

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -2,26 +2,67 @@ import mongoose, { Schema, Document } from "mongoose";
 import { UserType } from "../types/user.type";
 
 export interface IUser extends UserType, Document {
-    // can add mongo related attr
-    _id: mongoose.Types.ObjectId;
-    createdAt: Date;
-    updatedAt: Date;
+  _id: mongoose.Types.ObjectId;
+  resetPasswordToken?: string;
+  resetPasswordExpires?: Date;
+  createdAt: Date;
+  updatedAt: Date;
 }
+
 const UserMongoSchema: Schema = new Schema<IUser>(
-    {
-        firstName: { type: String, required: true },
-        lastName: { type: String, required: true },
-        email: { type: String, required: true, unique: true },
-        username: { type: String, required: true, unique: true },
-        password: { type: String, required: true },
-        role: { type: String, enum: ["admin", "user"], default: "user" }
+  {
+    fullName: {
+      type: String,
+      required: true,
     },
-    {
-        timestamps: true // createdAt and updatedAt will be automatically added and managed by mongoose
-    }
-)
-export const UserModel = mongoose.model<IUser>
-(
-    "User", // db.users -> Model Name "User"
-    UserMongoSchema
+
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+
+    contactNumber: {
+      type: String,
+      required: false,
+    },
+
+    gender: {
+      type: String,
+      enum: ["Male", "Female", "Other"],
+      required: false,
+    },
+
+    password: {
+      type: String,
+      required: true,
+    },
+
+    role: {
+      type: String,
+      enum: ["admin", "user"],
+      default: "user",
+    },
+
+    profileImage: {
+      type: String,
+      required: false,
+    },
+    resetPasswordToken: {
+      type: String,
+      required: false,
+    },
+    resetPasswordExpires: {
+      type: Date,
+      required: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const UserModel = mongoose.model<IUser>(
+  "User",
+  UserMongoSchema
 );
