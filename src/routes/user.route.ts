6f596c0a2d7c25ee86e1df8989ac4712ad87a427
jessica <jessicadhamala20@gmail.com>
2026-07-29 16:05:25 +0545

    Update user routes

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,10 +1,42 @@
 import { UserController } from "../controllers/user.controller";
 import { Router } from "express";
+import { authorizedMiddleware } from "../middlewares/authorized_middlewares";
+import { uploadProfileImage } from "../middlewares/upload_middleware";
 
 const userRouter = Router();
 const userController = new UserController();
 
-userRouter.post("/register", userController.createUser);
-userRouter.post("/login",userController.loginUser);
+userRouter.post("/register", userController.createUser.bind(userController));
+userRouter.post("/login", userController.loginUser.bind(userController));
+userRouter.post("/forgot-password", userController.forgotPassword.bind(userController));
+userRouter.post("/reset-password", userController.resetPassword.bind(userController));
+userRouter.get(
+    "/profile",
+    authorizedMiddleware,
+    userController.getProfile.bind(userController)
+);
+userRouter.patch(
+    "/profile/photo",
+    authorizedMiddleware,
+    uploadProfileImage.single("profileImage"),
+    userController.updateProfilePhoto.bind(userController)
+);
+userRouter.get(
+    "/whoami",
+    authorizedMiddleware,
+    userController.whoami.bind(userController)
+);
+userRouter.patch(
+    "/update",
+    authorizedMiddleware,
+    uploadProfileImage.single("profileImage"),
+    userController.updateProfile.bind(userController)
+);
+userRouter.put(
+    "/update",
+    authorizedMiddleware,
+    uploadProfileImage.single("profileImage"),
+    userController.updateProfile.bind(userController)
+);
 
 export default userRouter;
