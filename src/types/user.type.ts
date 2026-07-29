3ea09eaf5421c5057074fb7162f6325cb57e5dde
jessica <jessicadhamala20@gmail.com>
2026-07-29 16:02:13 +0545

    Update user types

--- src/types/user.type.ts
+++ src/types/user.type.ts
@@ -1,10 +1,22 @@
 import { z } from "zod";
+
 export const UserSchema = z.object({
-    firstName: z.string().min(1, "First name is required"),
-    lastName: z.string().min(1, "Last name is required"),
-    email: z.email("Invalid email address"),
-    username: z.string().min(3, "Username must be at least 3 characters long"),
-    password: z.string().min(6, "Password must be at least 6 characters long"),
-    role: z.enum(["admin", "user"]).default("user")
+  fullName: z.string().min(1, "Full name is required"),
+
+  email: z.email("Invalid email address"),
+
+  contactNumber: z.string().optional(),
+
+  gender: z.enum(["Male", "Female", "Other"]).optional(),
+
+  password: z.string().min(
+    6,
+    "Password must be at least 6 characters long"
+  ),
+
+  role: z.enum(["admin", "user"]).default("user"),
+
+  profileImage: z.string().optional(),
 });
+
 export type UserType = z.infer<typeof UserSchema>;
